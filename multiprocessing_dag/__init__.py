6a79886d2c4daf81b8bd8ca53d0efbd9eb483e51
Evan Davis <evandavis@lyft.com>
2019-09-09 12:39:09 -0700

    Fix bugs.

--- multiprocessing_dag/__init__.py
+++ multiprocessing_dag/__init__.py
@@ -5,6 +5,8 @@ import time
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_PROCESS_LIMIT = 4  # TODO get core count?
+
 
 class BaseCommand:
     def __init__(self):
@@ -80,20 +82,24 @@ class Process(multiprocessing.Process):
 
         self.task = task
         self.status_queue = status_queue
+        self.logger = logging.getLogger(task.uri())  # TODO uri?
 
     def run(self):
         success = True
         try:
             result = self.task.run()
             if not result:
+                self.logger.warning(f"Command failure in task '{self.task.uri()}'.")
                 success = False
-        except Exception:  # TODO let KeyboardInterrupt and others bubble up
+        except Exception as e:  # TODO let KeyboardInterrupt and others bubble up
+            self.logger.exception(f"Unhandled exception in task '{self.task.uri()}'.")
             success = False
         self.status_queue.put(success)
+        self.status_queue.close()
 
 
 class Runner:
-    def __init__(self, process_limit=4):  # TODO get core count? at least factor this in to a constant
+    def __init__(self, process_limit=DEFAULT_PROCESS_LIMIT):  # TODO set default value like this or do lookup?
         self.process_limit = process_limit
         self.queued_nodes = []
         self.finished_nodes = set()
@@ -112,16 +118,16 @@ class Runner:
         return None
 
     def _enqueue_pipeline(self, pipeline):
-        for upstream in next_node.upstreams:
-            for sub_node in next_node.nodes.values():
+        for upstream in pipeline.upstreams:
+            for sub_node in pipeline.nodes.values():
                 if not sub_node.upstreams:  # TODO shouldn't we add it in all cases?
-                    next_node.add_dependency(upstream, sub_node)
-        for downstream in next_node.downstreams:
-            for sub_node in next_node.nodes.values():
+                    pipeline.add_dependency(upstream, sub_node)
+        for downstream in pipeline.downstreams:
+            for sub_node in pipeline.nodes.values():
                 if not sub_node.downstreams:  # TODO shouldn't we add it in all cases?
-                    next_node.add_dependency(sub_node, downstream)
-        self.running_pipelines.add(next_node)
-        for sub_node in next_node.nodes.values():
+                    pipeline.add_dependency(sub_node, downstream)
+        self.running_pipelines.add(pipeline)
+        for sub_node in pipeline.nodes.values():
             self.queued_nodes.append(sub_node)
 
     def _start_task(self, task):
@@ -137,17 +143,25 @@ class Runner:
             # NOTE further testing needed here, I think
             if running_process.is_alive() and running_process.status_queue.empty():
                 continue
-            logger.info("Finishing task '{running_process.task.url()}'.")
+            logger.info(f"Finishing task '{running_process.task.uri()}'.")
             del self.running_tasks[running_process.task]
             self.finished_nodes.add(running_process.task)
-            failed = (running_process.exitcode != 0 or not running_process.status_queue.get())
+            exit_code = running_process.exitcode
+            status_result = running_process.status_queue.get()
+            if exit_code is None:
+                logger.info(f"Joining task '{running_process.task.uri()}'.")
+                running_process.join()  # TODO danger! no timeout!
+                exit_code = running_process.exitcode
+            failed = exit_code != 0 or not status_result
             if failed:
+                logger.warning(f"Failed task '{running_process.task.uri()}'.")
                 for parent in running_process.task.parents()[:-1]:
+                    logger.warning(f"Failing pipeline '{parent.uri()}'.")
                     self.failed_pipelines.add(parent)
 
     def _finish_pipelines(self):
         for running_pipeline in list(self.running_pipelines):
-            if set(running_pipelines.nodes.values()).issubset(self.finished_nodes):
+            if set(running_pipeline.nodes.values()).issubset(self.finished_nodes):
                 logger.info(f"Finishing pipeline '{running_pipeline.uri()}'.")
                 # failed = (running_pipeline in self.failed_pipelines)  # TODO
                 self.running_pipelines.remove(running_pipeline)
@@ -164,7 +178,7 @@ class Runner:
                     self._enqueue_pipeline(next_node)
                 elif isinstance(next_node, Task):
                     logger.info(f"Starting task '{next_node.uri()}'.")
-                    node_process = self._start_task(task)
+                    node_process = self._start_task(next_node)
                     self.running_tasks[next_node] = node_process
                 else:
                     pass  # NOTE happens when we dequeue None
